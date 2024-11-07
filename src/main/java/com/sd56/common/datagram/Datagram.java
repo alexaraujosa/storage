2e0723ef895711485c019415d2e19fffd6c9d377
alexaraujosa <alexaraujosa@hotmail.com>
2024-11-07 15:34:18 +0000

    feat(datagram): added initial implementation of the datagram

---
+++ src/main/java/com/sd56/common/datagram/Datagram.java
@@ -0,0 +1,45 @@
+package com.sd56.common.datagram;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Datagram {
+    private final static int version = 1;
+    private DatagramType type;
+
+    public enum DatagramType {
+        DATAGRAM_TYPE_REQUEST_AUTHENTICATION,
+        DATAGRAM_TYPE_REQUEST_PUT,
+        DATAGRAM_TYPE_REQUEST_GET,
+        DATAGRAM_TYPE_RESPONSE_AUTEHNTICATION,
+        DATAGRAM_TYPE_RESPONSE_PUT,
+        DATAGRAM_TYPE_RESPONSE_GET
+    };
+
+    public Datagram(DatagramType type) {
+        this.type = type;
+    }
+
+    public DatagramType getType() { return this.type; }
+
+    public String toString() {
+        return "{[DATAGRAM] | VERSION: " + version + " | TYPE: " + type + "}";
+    }
+
+    public void serialize(DataOutputStream out) throws IOException {
+        out.writeInt(this.version);
+        out.writeInt(this.type.ordinal());
+    }
+
+    public static Datagram deserialize(DataInputStream in) throws IOException {
+        int version = in.readInt();
+        if (version != Datagram.version) {
+            System.err.println("Invalid version value.");
+            // TODO: Melhorar error handling, não esquecendo de ler os bytes que faltam do datagrama.
+        }
+        DatagramType type = DatagramType.values()[in.readInt()];
+        return new Datagram(type);
+    }
+
+}
