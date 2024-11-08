4cf2b98d228e8be40b6e341820b9f42b11ee1700
Pauloarf <pauloferreira.vnc@gmail.com>
2024-11-08 10:34:12 +0000

    feat(db,threads,readme): adds a Readme file and creates a class ClientHanlder and dbHandler

---
+++ README.md
@@ -0,0 +1,16 @@
+# Distributed Systems Project
+
+**Group:** 56
+
+a104257, Alex Araújo de Sá, alexaraujosa  
+a100763, José Pedro Torres Vasconcelos, josevasconcelos2002
+a96268, Paulo Alexandre Rodrigues Ferreira, Pauloarf
+a104271, Rafael Santos Fernandes, DarkenLM
+
+## Setup
+
+## Documentation
+
+## VSCode Debugging
+
+## Recommended Extensions
--- src/main/java/com/sd56/server/Server.java
+++ src/main/java/com/sd56/server/Server.java
@@ -4,10 +4,46 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 import com.sd56.common.datagram.Datagram;
 
+class DatabaseManager {
+    private HashMap<String, byte[]> db = new HashMap<>();
+
+    public void update(String key) {
+
+    }
+}
+
+class ClientHandler implements Runnable {
+    private Socket socket;
+    private DatabaseManager dbManager;
+
+    public ClientHandler(Socket socket, DatabaseManager dbManager) {
+        this.socket = socket;
+        this.dbManager = dbManager;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello! Me am a Thread");
+
+        try{
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            while (!socket.isInputShutdown()) {
+                Datagram datagram = Datagram.deserialize(in);
+                System.out.println("Received: " + datagram.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
 public class Server {
+    private final static int PORT = 1337;
     private final int maxSessions;
 
     public Server(int maxSessions) {
@@ -21,18 +57,15 @@ public class Server {
         }
         Server server = new Server(Integer.parseInt(args[0]));
 
-        try (ServerSocket ss = new ServerSocket(1337)) {
+        try (ServerSocket ss = new ServerSocket(PORT)) {
             System.out.println("Server successfully created. Awaiting for connections...");
 
             while (true) {
                 Socket socket = ss.accept();
                 System.out.println("Client connection accepted.");
 
-                DataInputStream in = new DataInputStream(socket.getInputStream());
-                while (!socket.isInputShutdown()) {
-                    Datagram datagram = Datagram.deserialize(in);
-                    System.out.println("Received: " + datagram.toString());
-                }
+                Thread thread = new Thread(new ClientHandler(socket,new DatabaseManager()));
+                thread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
