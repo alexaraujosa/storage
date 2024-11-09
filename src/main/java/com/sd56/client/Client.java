d1ebbd42531b8406a90114affb74398acfa2b41f
alexaraujosa <alexaraujosa@hotmail.com>
2024-11-09 23:13:55 +0000

    feat(client): implemented authentication process and response interpretation for the Client solution

--- src/main/java/com/sd56/client/Client.java
+++ src/main/java/com/sd56/client/Client.java
@@ -1,11 +1,16 @@
 package com.sd56.client;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 import com.sd56.common.datagram.Datagram;
+import com.sd56.common.datagram.RequestAuthDatagram;
+import com.sd56.common.datagram.ResponseAuthDatagram;
+import com.sd56.common.datagram.ResponseGetDatagram;
+import com.sd56.common.datagram.ResponsePutDatagram;
 
 public class Client {
 
@@ -18,12 +23,39 @@ public class Client {
         String password = scanner.nextLine();
 
         try (Socket socket = new Socket("localhost", 1337)) {
-            System.out.println("Connection established.");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Connection established.");
 
-            Datagram datagram = new Datagram(Datagram.DatagramType.DATAGRAM_TYPE_REQUEST_AUTHENTICATION);
-            datagram.serialize(out);
-            System.out.println("Datagram sent.");
+            RequestAuthDatagram reqAuth = new RequestAuthDatagram(username, password);
+            reqAuth.serialize(out);
+            out.flush();
+            
+            while (!socket.isInputShutdown()) {
+                Datagram datagram = Datagram.deserialize(in);
+
+                switch (datagram.getType()) {
+                    case DATAGRAM_TYPE_RESPONSE_AUTHENTICATION:
+                        ResponseAuthDatagram resAuth = ResponseAuthDatagram.deserialize(in, datagram);
+                        if (resAuth.getValidation() == false) {
+                            System.err.println("Invalid credencials.");
+                            return;
+                        } 
+                        // TODO: Display Main Menu
+                        break;
+                    case DATAGRAM_TYPE_RESPONSE_PUT:
+                        ResponsePutDatagram resPut = ResponsePutDatagram.deserialize(in, datagram);
+                        // TODO
+                        break;
+                    case DATAGRAM_TYPE_RESPONSE_GET:
+                        ResponseGetDatagram resGet = ResponseGetDatagram.deserialize(in, datagram);
+                        // TODO
+                        break;
+                    default:
+                        // Just ignore it.
+                        break;
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
